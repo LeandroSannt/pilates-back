e6c8c0c3ffff2bd62e33376dfd2353237595148e
LeandroSannt <lsn_slim@yahoo.com.br>
2022-10-13 13:40:52 -0300

    ajustando schedul

--- app/Controllers/Http/FilesController.ts
+++ app/Controllers/Http/FilesController.ts
@@ -1,14 +1,32 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-//import FileServices from 'App/Services/FileServices'
+import { schema } from '@ioc:Adonis/Core/Validator';
+import FileServices from 'App/Services/FileServices';
 
 export default class FilesController {
 
-  async updateFileStudant({}:HttpContextContract){
-   // const files = request.file('prontuarios')
-    //const fileServices = new FileServices()
+  async updateFileStudant({request}:HttpContextContract){
+   const files = request.file('prontuarios')
+    const fileServices = new FileServices()
 
-    // if(files){
-    //   await fileServices.updateFileStudent(files)
-    // }
+    const postData = await request.validate({
+       schema:schema.create({
+        file:schema.file({
+          size:'2mb',
+          extnames:['pdf']
+        })
+       })
+    })
+
+    try{
+      // const i = await files?.moveToDisk('pilates',{},'s3')
+      const i = await postData.file.moveToDisk('teste',{},'s3')
+      console.log(i)
+    }catch(err){
+      console.log(err)
+    }
+
+    if(files){
+      await fileServices.updateFileStudent(files)
+    }
   }
 }
