b74b304440ff5e84d219feb933912046c373a9a8
LeandroSannt <lsn_slim@yahoo.com.br>
2022-10-14 23:40:12 -0300

    ajustes

--- app/Helpers/countMonths.ts
+++ app/Helpers/countMonths.ts
@@ -4,12 +4,14 @@ export const  countMonths = ({end,total}:any) => {
   var date1 = new Date(end)
   var date2 = new Date();
 
-  const diff = moment(date2, 'DD/MM/YYYY').diff(moment(date1, 'DD/MM/YYYY'))
-  const months = moment.duration(diff).asMonths()
-  const month = (Math.floor(months) * - 1 )
+  if(end){
+    const diff = moment(date2, 'DD/MM/YYYY').diff(moment(date1, 'DD/MM/YYYY'))
+    const months = moment.duration(diff).asMonths()
+    const month = (Math.floor(months) * - 1 )
 
-  let result = (total - month) + 1
-
-
-  return result
+    let result = (total - month) + 1
+    return result
+  }else{
+    return 0
+  }
 }
