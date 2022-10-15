bbec4aa43dfada41d4513d62b7bb91868554e4e5
LeandroSannt <lsn_slim@yahoo.com.br>
2022-10-14 23:03:27 -0300

    removendo validation

--- app/Validators/UpdateStudentValidator.ts
+++ app/Validators/UpdateStudentValidator.ts
@@ -36,7 +36,6 @@ export default class UpdateStudentValidator {
       rules.exists({table:'plans',column:"id"})
     ]),
     plan_expiration_day:schema.string(),
-    date_start_plan:schema.string(),
  })
 
   /**
